da08a60f9a80e319408044c16545d51bb9330307
Kirill Zagul <zagulkirill@gmail.com>
2022-03-29 13:19:22 +0500

    ORM

---
+++ core/app/db/Sequelize.js
@@ -0,0 +1,19 @@
+//DataBase ORM 
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize('travelblog', 'kirillzagul', 'vbrjkf', {
+    host: 'localhost',
+    dialect: 'postgres',
+    // operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+db.authenticate()
+    .then(() => console.log("DataBase connected..."))
+    .catch(err => console.log("Error: " + err))
