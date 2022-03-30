8a8e7841cf92011ab773ca33cb921873a32ddce0
Kirill Zagul <zagulkirill@gmail.com>
2022-03-30 17:12:36 +0500

    adding models and migration (with bags)

--- core/index.js
+++ core/index.js
@@ -2,29 +2,40 @@
 const express = require('express')
 const { Pool } = require('pg')
 const bodyParser = require("body-parser");
+const path = require('path');
 const cors = require('cors')
-
-//db
-const db = require('./app/db/Sequelize')
-
-//routes
-const typeofexcursion = require('./app/routes/typeofexcursion.routes')
-
 //app object
 const app = express()
+//db
+const db = require('./app/db/Sequelize')
+//models
+const {TypeOfExcursion} = require('./app/models/typeofexcursion.model')
+//routes
+const typeofexcursion = require('./app/routes/typeofexcursion.routes');
+const { json } = require('body-parser');
+
+
+const index = () => JSON.stringify(TypeOfExcursion.findAll())
+
+
+const addTypeOfExcursion = async (req) => {
+    const {text} = await json(req)
+    TypeOfExcursion.create({
+        text,
+    })
+}
+
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+  });
+
+
 
 
 //port for localhost
 const PORT = 3001
-
-app.get('/', async (req, res) => {
-    res.send("Heoo")
-})
-
-app.use('/api', typeofexcursion)
-
-
-
 //checking the server's work
 app.listen(PORT, () => {
     console.log(`listen to port ${PORT} \n\nhttp://localhost:3001/`)
