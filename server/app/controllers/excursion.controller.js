37ee0b49092d176e594e5886517a3494727a44ea
Kirill Zagul <zagulkirill@gmail.com>
2022-05-17 15:14:20 +0500

    crud fixed

--- server/app/controllers/excursion.controller.js
+++ server/app/controllers/excursion.controller.js
@@ -5,13 +5,26 @@ class ExcursionController {
     //API GET all Types of visiting
     async getAll(req, res, next) {
         try {
-            let sql = 'SELECT * from excursion'
-            // let sql = `SELECT * 
-            //             FROM excursion 
-            //             JOIN typeofexcursion 
-            //             ON excursion.typeexcursion_id = typeofexcursion.id
-            //             JOIN typeofvisiting 
-            //             ON excursion.typevisiting_id = typeofvisiting.id;`
+            // let sql = 'SELECT * from excursion'
+            let sql = `SELECT 
+                            excursion.id, 
+                            excursion.name, 
+                            excursion.location, 
+                            excursion.duration, 
+                            excursion.about, 
+                            typeofexcursion.typeexcursion,
+                            typeofvisiting.typevisiting
+                            
+                        FROM 
+                            excursion 
+                        JOIN 
+                            typeofexcursion 
+                        ON 
+                            excursion.typeexcursion_id = typeofexcursion.id
+                        JOIN 
+                            typeofvisiting 
+                        ON 
+                            excursion.typevisiting_id = typeofvisiting.id;`
             let result = await db.query(sql)
             res.json(result.rows)
         }
@@ -34,19 +47,20 @@ class ExcursionController {
             next(error)
         }
     }
-    // //API POST new locate
-    // async postLocate(req, res, next){
-    //     try{
-    //         const {address, longitude, latitude} = req.body
-    //         // console.log(name, surname)
-    //         const result = await db.query(`INSERT INTO locate (address, longitude, latitude) values($1, $2, $3) RETURNING *`, [address, longitude, latitude])
-    //         res.json(result.rows[0])
-    //     }
-    //     catch (error){
-    //         console.error(error)
-    //         next(error)
-    //     }
-    // }
+    //API POST new excursion
+    async postLocate(req, res, next){
+        try{
+            const {address, longitude, latitude} = req.body
+            // console.log(name, surname)
+            const result = await db.query(`INSERT INTO excursion (address, longitude, latitude) values($1, $2, $3) RETURNING *`, [address, longitude, latitude])
+            
+            res.json(result.rows[0])
+        }
+        catch (error){
+            console.error(error)
+            next(error)
+        }
+    }
     // //API PUT locate
     // async putLocate(req, res, next){
     //     try{
