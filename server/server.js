118a43bfe6fe3b7c89b88cddcd9ad314db63898b
Kirill Zagul <zagulkirill@gmail.com>
2022-05-23 23:11:27 +0500

    init

--- server/server.js
+++ server/server.js
@@ -79,6 +79,7 @@ require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3002;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
